6332ea287488eafbee512a87994c122efb6210fa
Fabrice Jossinet <fjossinet@gmail.com>
2015-09-15 15:36:26 +0200

    Removed the connection to a remote server for the test.

--- files/scripts/python/pyrna_tests.py
+++ files/scripts/python/pyrna_tests.py
@@ -6,10 +6,7 @@ A script to test the installation of PyRNA
 
 from pyrna.db import PDB
 from pyrna.parsers import parse_pdb, secondary_structure_to_base_pairs
-from pyrna.computations import Rnafold, Rnaview, get_api_key
-
-rest_server = "arn-ibmc.in2p3.fr"
-api_key = get_api_key(rest_server)
+from pyrna.computations import Rnafold, Rnaview
 
 def test():
     print "Recovering entry 1EHZ from Protein Databank...\n"
@@ -21,7 +18,7 @@ def test():
     print "List of base-pairs computed with RNAVIEW:\n"
 
     for ts in tertiary_structures:
-        secondary_structure, tertiary_structure = Rnaview(rest_server = rest_server, api_key = api_key).annotate(ts)
+        secondary_structure, tertiary_structure = Rnaview().annotate(ts)
         print secondary_structure_to_base_pairs(secondary_structure, keep_tertiaries = True)
 
     print "\n## 2D prediction ##\n"
@@ -30,7 +27,7 @@ def test():
         print "RNA sequence from 1EHZ:\n"
         print ts.rna.sequence
         print "\nList of base-pairs computed with RNAfold (RNA Vienna Package):\n"
-        print Rnafold(rest_server = rest_server, api_key = api_key).fold(molecule=ts.rna)
+        print Rnafold().fold(molecule=ts.rna)
 
 if __name__ == '__main__':
     test()
