b44884fcdcafe7621943fb63d0c0fe84af8ccc59
Fabrice Jossinet <fjossinet@gmail.com>
2016-04-03 11:53:32 +0200

    Minor test concerning a Docker issue

--- pyrna/computations.py
+++ pyrna/computations.py
@@ -1910,11 +1910,9 @@ class Rnaview(Tool):
                 else:
                     pdb_file.write(to_pdb(tertiary_structure, export_numbering_system = True))
 
-            commands.getoutput("echo $RNAVIEW")
-            commands.getoutput("more %s"%(pdb_file_name))
-            commands.getoutput("rnaview -p %s"%(pdb_file_name))
-
-            print commands.getoutput("more %s"%(pdb_file_name+".out"))
+            print commands.getoutput("echo $RNAVIEW")
+            print commands.getoutput("more %s"%(pdb_file_name))
+            print commands.getoutput("rnaview -p %s"%(pdb_file_name))
 
             xml_file_name = pdb_file_name+".xml"
             xml_content = ""
